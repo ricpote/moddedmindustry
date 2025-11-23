bf21510ad25db40fb6340d807bc30af321c05c4d
João <jpcb.fernandes@campus.fct.unl.pt>
2025-11-23 17:26:39 +0000

    Core Health Bar done

--- core/src/mindustry/ui/CoreHealthBar.java
+++ core/src/mindustry/ui/CoreHealthBar.java
@@ -15,6 +15,7 @@ public class CoreHealthBar extends Element {
     private static final Color COLOR_DEFAULT = Color.cyan;
     private static final Color COLOR_LOW_HEALTH = Color.red;
     private static final Color COLOR_TEXT = Color.white;
+    private static final float THICKNESS = 4f;
 
     public float barWidth = 250f;
     public float barHeight = 25f;
@@ -27,18 +28,22 @@ public class CoreHealthBar extends Element {
         if(!Vars.state.rules.waves || Vars.state.wave <= 1) return;
 
         CoreBlock.CoreBuild core = Vars.player.team().core();
+        if(core == null || core.dead()) return;
 
         float healthFrac = core.health() / core.maxHealth();
         float fillWidth = barWidth * healthFrac;
 
+        float fillX = x + fillWidth / 2f;
+        float fillY = y + barHeight / 2f;
+
         Color healthColor = healthFrac > 0.33 ? COLOR_DEFAULT : COLOR_LOW_HEALTH;
 
         Draw.color(COLOR_BACKGROUND);
-        Lines.stroke(2f);
+        Lines.stroke(THICKNESS);
         Lines.rect(x, y, barWidth, barHeight);
 
         Draw.color(healthColor);
-        Fill.rect(x, y, fillWidth, barHeight);
+        Fill.rect(fillX, fillY, Math.max(0f, fillWidth - 2 * THICKNESS), barHeight - 2 * THICKNESS);
 
         Draw.color(COLOR_TEXT);
         Fonts.outline.draw("Core Health", x, y + height + 25f);
